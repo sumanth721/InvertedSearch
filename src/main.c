53b78e4b26c7d3aae8025c167f09638793d07b87
sumanth <sumanth.sarvepalli@gmail.com>
2022-04-08 11:31:35 +0530

    Initial Commit

---
+++ src/main.c
@@ -0,0 +1,18 @@
+/*
+ *
+ * Title        : Inverted Search
+ * Author       : Sumanth
+ * Description  : This Project is to develop a search tool
+ * Tools Used   : Data Structures
+ *
+ */
+
+
+#include <stdio.h>
+
+int main()
+{
+    printf("Welcome to Inverted Search Project\n");
+
+    return 0;
+}
